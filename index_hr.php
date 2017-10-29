0ca87a58ca40742eb417e1ce5c54fe23eb79a4dd
Jakov M. Vežić <jakov@vezic.eu>
2017-10-29 20:29:54 +0100

    Metapodaci

--- index_hr.php
+++ index_hr.php
@@ -8,6 +8,11 @@
 	<head>
 		<title>Morepress | Sveučilište u Zadru</title>
 		<meta charset="utf-8" />
+
+		<meta name="description" content="Mrežna platforma digitalnih publikacija Sveučilišta u Zadru">
+		<meta name="keywords" content="Morepress, platforma, Sveučilište u Zadru, znanosti, znanstveni časopisi, izdavaštvo, knjige, OJS, Open Journal System, OMP, Open Monograph Press">
+		<meta name="author" content="Sveučilište u Zadru">
+
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
