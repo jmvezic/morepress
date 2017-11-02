1d818dbaba9a9bb3879826dd968b04651a5d2d89
Jakov <jakov@vezic.eu>
2017-11-02 11:30:41 +0100

    Piwik na glavnom indexu

--- index_hr.php
+++ index_hr.php
@@ -18,6 +18,8 @@
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<script type="text/javascript" src="piwik.js"></script>
+
 	</head>
 	
 	<?php 
